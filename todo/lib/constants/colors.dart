21b31faa9e24211a0757481d4e6ad2cdb96ee13e
MariamMoselhi <mariamalaa3322@gmail.com>
2026-02-28 01:57:02 +0200

    add to-do-list app

---
+++ todo/lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color.fromARGB(206, 185, 49, 49);
+const Color tdBlue = Color.fromARGB(211, 29, 29, 122);
+const Color tdBlack = Color.fromARGB(255, 55, 56, 55);
+const Color tdBGColor = Color.fromARGB(255, 216, 219, 234);
+const Color tdGrey =  Color.fromARGB(233, 128, 128, 128);
